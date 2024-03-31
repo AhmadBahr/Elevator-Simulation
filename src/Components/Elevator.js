739a1903515c21ce8587e0acc793fd23dcb02da1
Ahmad <ahmad.bahr@mubs.edu.lb>
2024-03-31 23:47:26 +0300

    setup the elevators to move randomly

---
+++ src/Components/Elevator.js
@@ -0,0 +1,55 @@
+import React, {useState, useEffect} from 'react'
+import {useSelector} from 'react-redux'
+import './Elevator.css'
+
+const Elevator = ({elevatorID}) => {
+    const elevators = useSelector(state => state.elevators);
+    const [currentFloor, setCurrentFloor] = useState(1);
+    const [direction, setDirection] = useState(null);
+    const [isMoving, setISMoving] = useState(false);
+    const [blink,setBlink] = useState(false);
+
+    const moveElevator = () => { 
+        setISMoving(true);
+        setTimeout(() => {
+            let nextFloor;
+            let nextDirection = direction;
+
+            if (currentFloor === 1) {
+                nextDirection = "up";
+            } else if (currentFloor === 20) {
+                nextDirection = "down";
+            } else{
+                nextDirection = Math.random() < 0.5 ? "up" : "down";
+            }
+
+            if (nextDirection === "up" && currentFloor < 20) {
+                nextFloor = currentFloor + 1;
+            } else if(nextDirection ==='down' && currentFloor > 1 ) {
+                nextFloor = currentFloor - 1;
+            } else {
+                setIsMoving(false);
+                return;
+            }
+
+            setCurrentFloor(nextFloor);
+            setDirection(nextDirection);
+
+            setISMoving(false);
+            setDirection(nextDirection);
+
+            setISMoving(false);
+            console.log(`Elevator ${elevatorId} is now on floor ${nextFloor}`);
+            }, 2000);
+        };
+        useEffect(() => {
+            if(isMoving) {
+                console.log(`Elevator ${elevatorId} is moving ${direction} to floor ${currentFloor + (direction == ' up' ? 1 : -1)}`);
+            }
+        }, {isMoving, curentFloor,direction, elevatorId});
+  return (
+    <div>Elevator</div>
+  )
+}
+
+export default Elevator
