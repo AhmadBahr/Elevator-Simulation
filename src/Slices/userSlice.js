dc64a20b65a08a654084b29234f5819eec7d85bd
Ahmad <ahmad.bahr@mubs.edu.lb>
2024-04-02 00:32:17 +0300

    userReducers

--- src/Slices/userSlice.js
+++ src/Slices/userSlice.js
@@ -6,3 +6,18 @@ const userSlice = createSlice({
       currentFloor: null,
       pressedButtons: [],
     },
+    reducers: {
+        enterBuilding(state, action) {
+          state.currentFloor = 1;
+        },
+        pressButton(state, action) {
+          const buttonPressed = action.payload;
+          state.pressedButtons = [...state.pressedButtons, buttonPressed];
+        },
+        clearButtons(state) {
+          state.pressedButtons = [];
+        },
+      },
+    });
+    export const { enterBuilding, pressButton, clearButtons } = userSlice.actions;
+    export default userSlice.reducer;
