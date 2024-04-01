aa0fa3cbb35094cd9f025a6f20b61aed279eab7b
Ahmad <ahmad.bahr@mubs.edu.lb>
2024-04-01 16:50:48 +0300

    calculateClosestElevator

--- src/Components/ElevatorControlPanel.js
+++ src/Components/ElevatorControlPanel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {useDispatch, useSelector} from 'react-redux';
 import {moveToFloor} from '../Slices/elevatorSlice';
 
@@ -6,4 +6,43 @@ const ElevatorControlPanel = () => {
     const dispatch = useDispatch();
     const elevators = useSelector(state => state.elevators);
     const {disabled} = useSelector (state => state.elevatorPanel);
-}
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const[ floorInput] = useState('');
+
+    const calculateClosestElevator = (userFloor) => {
+        let closestElevator = null;
+        let minDistance = Number.MAX_VALUE;
+
+        elevators.forEach(elevator => {
+            const distance = Math.abs(elevbator.currentFloor - userFloor);
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestElevator = elevator;
+            }
+        });
+        return closestElevator;
+    };
+    const handleButtonPress = (userFloor) => {
+        if (!disabled && !buttonDisabled) {
+          const closestElevator = calculateClosestElevator(userFloor);
+          
+          if (closestElevator) {
+    
+            alert(`Elevator ${closestElevator.id} is coming to floor ${userFloor}.`);
+    
+            dispatch(moveToFloor({ elevatorId: closestElevator.id, floor: userFloor }));
+            setButtonDisabled(true);
+            setTimeout(() => {
+              setButtonDisabled(false);
+    
+              alert(`Elevator ${closestElevator.id} has arrived at floor ${userFloor}.`);
+            }, 5000);
+          } else {
+            alert('No available elevators at the moment.');
+          }
+        } else {
+          alert("Elevator panel is disabled. Please wait for the elevator to arrive.");
+        }
+      };
+      
+    }    
