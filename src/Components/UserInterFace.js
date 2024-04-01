7e04dc35e04a585ab4470789e774c5dfe7f3c385
Ahmad <ahmad.bahr@mubs.edu.lb>
2024-04-01 19:13:27 +0300

    adding labels on elevators

--- src/Components/UserInterFace.js
+++ src/Components/UserInterFace.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { enterBuilding } from '../Slices/userSlice';
+import Elevator from './Elevator';
+import ElevatorControlPanel from './ElevatorControlPanel';
 
-const UserInterFace = () => {
-  return (
-    <div>UserInterFace</div>
-  )
-}
+const UserInterface = () => {
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      dispatch(enterBuilding());
+    }, [dispatch]);
+  
+    const elevatorIds = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']; 
 
-export default UserInterFace
+
+    return (
+        <div className="user-interface">
+          <h1>Smart Elevator System</h1>
+          <div className="elevator-container">
+            {elevatorIds.map(id => <Elevator key={id} elevatorId={id} />)}
+          </div>
+          <ElevatorControlPanel />
+        </div>
+      );
+    };
+     export default UserInterFace;
