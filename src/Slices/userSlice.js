d0c4ec6281f4b049ba54a0674254a43472d59807
Ahmad <ahmad.bahr@mubs.edu.lb>
2024-04-02 00:16:14 +0300

    create user Slice

--- src/Slices/userSlice.js
+++ src/Slices/userSlice.js
@@ -1,2 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+      currentFloor: null,
+      pressedButtons: [],
+    },
