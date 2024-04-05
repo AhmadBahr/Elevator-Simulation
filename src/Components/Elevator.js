75d315864a24d65627f27c4d41c05717ef885d19
Ahmad <ahmad.bahr@mubs.edu.lb>
2024-04-05 23:15:21 +0300

    some changes

--- src/Components/Elevator.js
+++ src/Components/Elevator.js
@@ -1,71 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import './Elevator.css';
 
 const Elevator = ({ elevatorId }) => {
   const elevators = useSelector(state => state.elevator.elevators);
-  const [currentFloor, setCurrentFloor] = useState(1);
-  const [direction, setDirection] = useState('up');
+  const [currentFloor, setCurrentFloor] = useState(1); 
+  const [direction, setDirection] = useState('up')
   const [isMoving, setIsMoving] = useState(false);
   const [blink, setBlink] = useState(false);
-  const [audio, setAudio] = useState(null); 
 
-  useEffect(() => {
-    import('../assets/ding.mp3').then(module => {
-      setAudio(new Audio(module.default));
-    }).catch(error => {
-      console.error('Error loading sound file:', error);
-    });
-  }, []); 
 
-  const moveElevator = () => {
-    setIsMoving(true);
-    setTimeout(() => {
-      let nextFloor;
-      let nextDirection = direction;
+const moveElevator = () => {
+  setIsMoving(true);
+  setTimeout(() => {
+    let nextFloor;
+    let nextDirection = direction; 
 
-      if (currentFloor === 1) {
-        nextDirection = 'up';
-      } else if (currentFloor === 20) {
-        nextDirection = 'down';
-      } else {
-        nextDirection = Math.random() < 0.5 ? 'up' : 'down';
-      }
+    if (currentFloor === 1) {
+      nextDirection = 'up'; 
+    } else if (currentFloor === 20) {
+      nextDirection = 'down';
+    } else {
+      nextDirection = Math.random() < 0.5 ? 'up' : 'down';
+    }
 
-      if (nextDirection === 'up' && currentFloor < 20) {
-        nextFloor = currentFloor + 1;
-      } else if (nextDirection === 'down' && currentFloor > 1) {
-        nextFloor = currentFloor - 1;
-      } else {
-        setIsMoving(false);
-        return;
-      }
 
-      setCurrentFloor(nextFloor);
-      setDirection(nextDirection);
-
+    if (nextDirection === 'up' && currentFloor < 20) {
+      nextFloor = currentFloor + 1;
+    } else if (nextDirection === 'down' && currentFloor > 1) {
+      nextFloor = currentFloor - 1;
+    } else {
       setIsMoving(false);
-      console.log(`Elevator ${elevatorId} is now at floor ${nextFloor}`);
+      return; 
+    }
 
-      if (audio) {
-        audio.play();
-      }
-    }, 2000);
-  };
+
+    setCurrentFloor(nextFloor);
+    setDirection(nextDirection);
+
+    setIsMoving(false);
+    console.log(`Elevator ${elevatorId} is now at floor ${nextFloor}`);
+  }, 2000); 
+};
+
 
   useEffect(() => {
     if (isMoving) {
-      console.log(
-        `Elevator ${elevatorId} is moving ${direction} to floor ${
-          currentFloor + (direction === 'up' ? 1 : -1)
-        }`
-      );
+    
+      console.log(`Elevator ${elevatorId} is moving ${direction} to floor ${currentFloor + (direction === 'up' ? 1 : -1)}`);
     }
   }, [isMoving, currentFloor, direction, elevatorId]);
 
-  const handleButtonPress = userFloor => {
+
+  const handleButtonPress = (userFloor) => {
     let closestElevator = null;
     let minDistance = Number.MAX_VALUE;
-
+    
     elevators.forEach(elevator => {
       const distance = Math.abs(elevator.currentFloor - userFloor);
       if (distance < minDistance) {
@@ -73,21 +63,17 @@ const Elevator = ({ elevatorId }) => {
         closestElevator = elevator;
       }
     });
-
+    
     if (closestElevator) {
       alert(`Elevator ${closestElevator.id} is coming to floor ${userFloor}.`);
-
       closestElevator.currentFloor = userFloor;
-      setBlink(true);
-      setTimeout(() => {
-        setBlink(false);
-      }, 2000);
     } else {
       alert('No available elevators at the moment.');
     }
   };
 
   useEffect(() => {
+
     moveElevator();
   }, [currentFloor]);
 
